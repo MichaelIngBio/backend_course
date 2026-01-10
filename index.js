d5a6956e42b5c7bd33d88e3359dde0380f0a508f
USUARIO <michael3135056851@gmail.com>
2026-01-10 18:35:03 -0500

    uibuild

--- index.js
+++ index.js
@@ -37,12 +37,12 @@ app.get('/api/notes/:id', (request, response) => {
 app.post('/api/notes', (request, response) => {
     const body = request.body
 
-    if(!boddy.content){
+    if(!body.content){
         return response.status(400).json({error: 'content missing' })
     }
 
     const note = new Note({
-        content: boddy.content,
+        content: body.content,
         important: body.important || false,
     })
 
