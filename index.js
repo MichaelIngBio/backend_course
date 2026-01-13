af50fa4a6e601d2c76b39ff45c9632272492bae3
USUARIO <michael3135056851@gmail.com>
2026-01-13 17:12:36 -0500

    uibuild

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     
     if(error.name === 'CastError'){
-        return response.status(400).send({error: 'malformatted id'})
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if(error.name === 'ValidationError'){
+        return response.status(400).json({ error: error.message })
     }
     next(error)
 }
@@ -49,8 +51,12 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
+
+    if(body.content === undefined){
+        return responsse.status(400).json({error: 'content missing'})
+    }
 
     if(!body.content){
         return response.status(400).json({error: 'content missing' })
@@ -61,20 +67,17 @@ app.post('/api/notes', (request, response) => {
         important: body.important || false,
     })
 
-    note.save().then((savedNote) =>{
-        response.json(savedNote)
-    })
+    note.save()
+        .then((savedNote) =>{
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) =>{
-    const body = request.body
+    const { content, important } = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    Note.findByIdAndUpdate(request.params.id, { content, important }, {new: true, runValidators: true, context: 'query'})
         .then(updatedNote => {
             response.json(updatedNote)
         })
@@ -82,7 +85,7 @@ app.put('/api/notes/:id', (request, response, next) =>{
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIddAndDelete(request.params.id)
+    Note.findByIdAndDelete(request.params.id)
         .then(result =>{
             response.status(204).end()
         })
